4b5b1822bcf8301610238ada0f41272dfec94137
Kgaogelo <kgaogelojoseph33@gmail.com>
2026-06-09 02:08:28 +0200

    added the whitenoise package for better view of my api

--- backend/memberssistant/settings.py
+++ backend/memberssistant/settings.py
@@ -35,10 +35,11 @@ INSTALLED_APPS = [
     'django_filters',
     'api',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  
     'django.middleware.security.SecurityMiddleware', 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
